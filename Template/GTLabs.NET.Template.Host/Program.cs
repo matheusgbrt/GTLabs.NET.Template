5a7b09bdc831fc292db88cc393e687c1cb0c7292
Matheus Guedes Bertaiolli <70043048+matheusgbrt@users.noreply.github.com>
2025-12-01 18:29:50 -0300

    feat: template progression

--- Template/GTLabs.NET.Template.Host/Program.cs
+++ Template/GTLabs.NET.Template.Host/Program.cs
@@ -1,19 +1,27 @@
+using Gtlabs.Api.AmbientData;
 using Gtlabs.Api.Extensions;
 using Gtlabs.AppRegistration.Extensions;
 using Gtlabs.Consul.Extensions;
 using Gtlabs.DependencyInjections.DependencyInjectons.Extensions;
+using GTLabs.NET.Template.Infrastructure.Contexts;
+using Gtlabs.Persistence.Extensions;
 using Gtlabs.Redis.Extensions;
 using Gtlabs.ServiceBus.ServiceBus.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.RegisterApp("GTLabs.NET.Template");
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddAmbientData();
 builder.Services.RegisterAllDependencies();
 await builder.Configuration.AddConsulConfigurationAsync();
 builder.Services.AddConsulRegistration(builder.Configuration);
 builder.ConfigureKestrelWithNetworkHelper();
 builder.Services.RegisterServiceBus(builder.Configuration);
-builder.Services.AddRedisCache();
+builder.Services.AddRedisCache(builder.Configuration);
+
+builder.Services.AddPersistence<TemplateDbcontext>(builder.Configuration);
+
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
---
+++ Template/GTLabs.NET.Template.Infrastructure/Contexts/DbContext.cs
@@ -0,0 +1,16 @@
+﻿using Gtlabs.Api.AmbientData;
+using Gtlabs.Persistence.CustomDbContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace GTLabs.NET.Template.Infrastructure.Contexts;
+
+public class TemplateDbcontext : GtLabsDbContext
+{
+    public TemplateDbcontext(DbContextOptions options, IAmbientData ambientData) : base(options, ambientData)
+    {
+    }
+    
+    public class IdentityDbContextFactory : GtLabsDbContextFactory<TemplateDbcontext>
+    {
+    }
+}
