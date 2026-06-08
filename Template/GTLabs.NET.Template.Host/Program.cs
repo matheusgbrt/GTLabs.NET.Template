77bf1373007b17eeb341a377682d17e22f03b63e
Matheus Bertaiolli <matheusgbadm@gmail.com>
2026-06-07 21:11:43 -0300

    Template alterations

--- Template/GTLabs.NET.Template.Host/Program.cs
+++ Template/GTLabs.NET.Template.Host/Program.cs
@@ -1,8 +1,9 @@
-using Gtlabs.Api.AmbientData;
+using Gtlabs.Authentication.Extensions;
 using Gtlabs.Api.Extensions;
+using Gtlabs.AspNet.Extensions;
 using Gtlabs.AppRegistration.Extensions;
 using Gtlabs.Consul.Extensions;
-using Gtlabs.DependencyInjections.DependencyInjectons.Extensions;
+using Gtlabs.Logging.Extensions;
 using GTLabs.NET.Template.Infrastructure.Contexts;
 using Gtlabs.Persistence.Extensions;
 using Gtlabs.Redis.Extensions;
@@ -11,19 +12,22 @@ using Gtlabs.ServiceBus.ServiceBus.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.RegisterApp("GTLabs.NET.Template");
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddAmbientData();
-builder.Services.RegisterAllDependencies();
 await builder.Configuration.AddConsulConfigurationAsync();
-builder.Services.AddConsulRegistration(builder.Configuration);
+
+builder.Host.AddBasicFeatures();
 builder.ConfigureKestrelWithNetworkHelper();
+
+builder.Services.AddBasicFeatures(builder.Configuration);
+builder.Services.AddGtlabsTracing(builder.Configuration);
+builder.Services.AddGtlabsAuthentication(options =>
+{
+    options.AllowAppAndUserTokens();
+});
+builder.Services.AddConsulRegistration(builder.Configuration);
 builder.Services.RegisterServiceBus(builder.Configuration);
 builder.Services.AddRedisCache(builder.Configuration);
-
 builder.Services.AddPersistence<TemplateDbcontext>(builder.Configuration);
 
-
-builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -33,9 +37,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
-app.AddConsulHealthCheck();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
--- Template/GTLabs.NET.Template.Infrastructure/Contexts/TemplateDbContext.cs
+++ Template/GTLabs.NET.Template.Infrastructure/Contexts/TemplateDbContext.cs
@@ -1,12 +1,14 @@
-﻿using Gtlabs.Api.AmbientData;
-using Gtlabs.Persistence.CustomDbContext;
+﻿using Gtlabs.Persistence.CustomDbContext;
+using Gtlabs.Persistence.DataFilters;
+using Gtlabs.Persistence.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace GTLabs.NET.Template.Infrastructure.Contexts;
 
 public class TemplateDbcontext : GtLabsDbContext
 {
-    public TemplateDbcontext(DbContextOptions options, IAmbientData ambientData) : base(options, ambientData)
+    public TemplateDbcontext(
+        DbContextOptions<TemplateDbcontext> options) : base(options)
     {
     }
     
